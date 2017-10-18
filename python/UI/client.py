8bd73e5ce4b18efd8cf8494039d88142caa18606
surya3997 <surya3997@gmail.com>
2017-10-18 14:09:55 +0530

    UI file created with pyqt

---
+++ python/UI/client.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'client.ui'
+#
+# Created by: PyQt4 UI code generator 4.11.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName(_fromUtf8("Form"))
+        Form.resize(354, 300)
+        self.gridLayout = QtGui.QGridLayout(Form)
+        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
+        self.horizontalLayout_4 = QtGui.QHBoxLayout()
+        self.horizontalLayout_4.setObjectName(_fromUtf8("horizontalLayout_4"))
+        self.lineEdit_5 = QtGui.QLineEdit(Form)
+        self.lineEdit_5.setObjectName(_fromUtf8("lineEdit_5"))
+        self.horizontalLayout_4.addWidget(self.lineEdit_5)
+        self.pushButton_5 = QtGui.QPushButton(Form)
+        self.pushButton_5.setObjectName(_fromUtf8("pushButton_5"))
+        self.horizontalLayout_4.addWidget(self.pushButton_5)
+        self.gridLayout.addLayout(self.horizontalLayout_4, 2, 0, 1, 1)
+        self.horizontalLayout_3 = QtGui.QHBoxLayout()
+        self.horizontalLayout_3.setObjectName(_fromUtf8("horizontalLayout_3"))
+        self.lineEdit_4 = QtGui.QLineEdit(Form)
+        self.lineEdit_4.setObjectName(_fromUtf8("lineEdit_4"))
+        self.horizontalLayout_3.addWidget(self.lineEdit_4)
+        self.pushButton_4 = QtGui.QPushButton(Form)
+        self.pushButton_4.setObjectName(_fromUtf8("pushButton_4"))
+        self.horizontalLayout_3.addWidget(self.pushButton_4)
+        self.gridLayout.addLayout(self.horizontalLayout_3, 1, 0, 1, 1)
+        self.horizontalLayout = QtGui.QHBoxLayout()
+        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
+        self.lineEdit_2 = QtGui.QLineEdit(Form)
+        self.lineEdit_2.setObjectName(_fromUtf8("lineEdit_2"))
+        self.horizontalLayout.addWidget(self.lineEdit_2)
+        self.pushButton_2 = QtGui.QPushButton(Form)
+        self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
+        self.horizontalLayout.addWidget(self.pushButton_2)
+        self.gridLayout.addLayout(self.horizontalLayout, 0, 0, 1, 1)
+        self.pushButton = QtGui.QPushButton(Form)
+        self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        self.gridLayout.addWidget(self.pushButton, 3, 0, 1, 1)
+        self.lineEdit_2.raise_()
+        self.pushButton.raise_()
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(_translate("Form", "Form", None))
+        self.pushButton_5.setText(_translate("Form", "Message", None))
+        self.pushButton_4.setText(_translate("Form", "Port", None))
+        self.pushButton_2.setText(_translate("Form", "IP", None))
+        self.pushButton.setText(_translate("Form", "Back", None))
+
